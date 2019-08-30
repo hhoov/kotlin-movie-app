66cda2a04a22a975eb2e476a32dbad021703f716
Hannah Hoover <hannahghoover@gmail.com>
2019-08-29 21:47:25 -0500

    Added URL for raw json movie details + temporary URL as test image for Glide, as the current image links give Error 404.

--- app/src/main/java/com/example/kotlin_movie_app/util/Constants.kt
+++ app/src/main/java/com/example/kotlin_movie_app/util/Constants.kt
@@ -3,5 +3,7 @@ package com.example.kotlin_movie_app.util
 class Constants {
     companion object {
         const val BASE_URL: String = "https://raw.githubusercontent.com/MercuryIntermedia/Sample_Json_Movies/master/top_movies.json"
+        const val DETAIL_URL : String = "https://raw.githubusercontent.com/MercuryIntermedia/Sample_Json_Movies/master/by_id/"
+        const val TEST_IMAGE_URL : String = "https://static.techspot.com/images2/downloads/topdownload/2019/07/2019-07-17-ts3_thumbs-9d7.png"
     }
 }

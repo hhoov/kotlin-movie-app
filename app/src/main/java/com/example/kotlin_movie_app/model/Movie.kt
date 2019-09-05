ef7fbd3cfbf90438410e18f0f200b41c74e2a6d4
Hannah Hoover <hannahghoover@gmail.com>
2019-09-05 15:57:10 -0500

    Took out extra info in Movie model that will be included in the new MovieDetails model.

--- app/src/main/java/com/example/kotlin_movie_app/model/Movie.kt
+++ app/src/main/java/com/example/kotlin_movie_app/model/Movie.kt
@@ -10,31 +10,9 @@ data class Movie
 @JsonCreator
 constructor(
     @param:JsonProperty("poster") val poster: String,
+    @param:JsonProperty("imdbId") val imdbId : String,
     @param:JsonProperty("rank") val rank: Int,
     @param:JsonProperty("title") val title: String,
     @param:JsonProperty("year") val year: Int
 
-)
-
-data class MovieDetails
-@JsonCreator
-constructor(
-    @param:JsonProperty("imdbRating") val imdbRating: Double,
-    @param:JsonProperty("imdbVotes") val imdbVotes: Int,
-    @param:JsonProperty("imdbId") val imdbId: String,
-    @param:JsonProperty("title") val title: String,
-    @param:JsonProperty("year") val year: Int,
-    @param:JsonProperty("rated") val rated: String,
-    @param:JsonProperty("released") val released: String,
-    @param:JsonProperty("runtime") val runtime: String,
-    @param:JsonProperty("genre") val genre: ArrayList<String>,
-    @param:JsonProperty("director") val director: String,
-    @param:JsonProperty("writer") val writer: String,
-    @param:JsonProperty("actors") val actors: ArrayList<String>,
-    @param:JsonProperty("plot") val plot: String,
-    @param:JsonProperty("language") val language: ArrayList<String>,
-    @param:JsonProperty("country") val country: String,
-    @param:JsonProperty("awards") val awards: String,
-    @param:JsonProperty("poster") val poster: String,
-    @param:JsonProperty("metascore") val metascore: Int
 )
---
+++ app/src/main/java/com/example/kotlin_movie_app/ui/MovieSummaryPresenter.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin_movie_app.ui
+
+class MovieSummaryPresenter {
+}
